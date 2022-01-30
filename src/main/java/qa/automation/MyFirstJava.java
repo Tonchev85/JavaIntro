7d52f4e964e54bce6dc9afd390b1c351c70e104b
Emil <emilchy1985@gmail.com>
2022-01-30 15:44:53 +0200

    Added my first project

--- src/main/java/qa/automation/MyFirstJava.java
+++ src/main/java/qa/automation/MyFirstJava.java
@@ -1,4 +1,29 @@
 package qa.automation;
 
+import jdk.jshell.SourceCodeAnalysis;
+
+import java.util.Scanner;
+
 public class MyFirstJava {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter your First name");
+
+        String firstName = scanner.next();
+
+        System.out.println("Please enter your Last name");
+
+        String lastName = scanner.next();
+
+        System.out.println("Please enter your hobby");
+
+        String hobby = scanner.next();
+
+        System.out.println("Your name is " + firstName + " " + lastName +
+                " and your favorite hobby is " + hobby);
+
+
+    }
 }
